8bcc24e5da49698555b910739fcdb51b99e529d3
samsgrafd <samsgrafd@gmail.com>
2015-01-24 13:20:23 +0200

    sam scripts

---
+++ Rockets/Assets/Scripts/Health.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+    
+    public int health;
+    int points;
+
+    void damages()
+    {
+        health--;
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Enemy")
+        {
+            damages();
+            points = +1; 
+        }
+
+        if (col.gameObject.tag == "Missile")
+        {
+            damages();
+            Destroy(col.gameObject);
+        }
+    
+    }
+
+    
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
