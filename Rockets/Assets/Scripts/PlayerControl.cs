04b14d97c9bfc16828be6785c058cfd40a549b73
Karri Ojala <ojala.karri@gmail.com>
2015-01-24 12:44:01 +0200

    New files

--- Rockets/Assets/Scripts/PlayerControl.cs
+++ Rockets/Assets/Scripts/PlayerControl.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 	public float speed;
-
+	public GameObject bullet;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,19 +11,44 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		rigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*speed, Input.GetAxisRaw("Vertical")*speed);
-
-
+		if (Input.GetKeyDown(KeyCode.I)) {
+			shoot("up");
+		}
+		if (Input.GetKeyDown(KeyCode.J)) {
+			shoot("left");
+		}
+		if (Input.GetKeyDown(KeyCode.K)) {
+			shoot("down");
+		}
+		if (Input.GetKeyDown(KeyCode.L)) {
+			shoot("right");
+		}
 	}
 
-	void OnCollisionEnter2D(Collision2D coll) {
-		if (coll.gameObject.tag == "Enemy") {
-			Die (coll.gameObject);
+	void shoot(string direction) {
+		switch (direction) {
+		case "up":
+			GameObject newbullet1 = (GameObject)Instantiate(bullet);
+			newbullet1.transform.position = transform.position;
+			newbullet1.rigidbody2D.velocity = new Vector2(0f, 6f);
+			break;
+		case "left":
+			GameObject newbullet2 = (GameObject)Instantiate(bullet);
+			newbullet2.transform.position = transform.position;
+			newbullet2.rigidbody2D.velocity = new Vector2(-6f, 0f);
+			break;
+		case "down":
+			GameObject newbullet3 = (GameObject)Instantiate(bullet);
+			newbullet3.transform.position = transform.position;
+			newbullet3.rigidbody2D.velocity = new Vector2(0f, -6f);
+			break;
+		case "right":
+			GameObject newbullet4 = (GameObject)Instantiate(bullet);
+			newbullet4.transform.position = transform.position;
+			newbullet4.rigidbody2D.velocity = new Vector2(6f, 0f);
+			break;
 				}
-	}
-
-	void Die(GameObject coll) {
-		Destroy (coll);
-		Destroy (gameObject);
 	}
 }
